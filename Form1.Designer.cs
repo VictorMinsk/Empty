1f1ab6605d3cba64512341a6b89a570d0793ba6a
VictorMinsk <vic.nic202@gmail.com>
2022-12-18 11:30:48 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace Lab5Graphic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnNumberOfPoints = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.arguments = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.values = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnInterpolate = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnNumberOfPoints
+            // 
+            this.btnNumberOfPoints.Location = new System.Drawing.Point(29, 69);
+            this.btnNumberOfPoints.Name = "btnNumberOfPoints";
+            this.btnNumberOfPoints.Size = new System.Drawing.Size(230, 36);
+            this.btnNumberOfPoints.TabIndex = 0;
+            this.btnNumberOfPoints.Text = "Изменить число точек";
+            this.btnNumberOfPoints.UseVisualStyleBackColor = true;
+            this.btnNumberOfPoints.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(29, 28);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(230, 26);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.arguments,
+            this.values});
+            this.dataGridView1.Location = new System.Drawing.Point(29, 154);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 50;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(230, 225);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // arguments
+            // 
+            this.arguments.HeaderText = "x";
+            this.arguments.MinimumWidth = 50;
+            this.arguments.Name = "arguments";
+            this.arguments.Width = 52;
+            // 
+            // values
+            // 
+            this.values.HeaderText = "f(x)";
+            this.values.MinimumWidth = 50;
+            this.values.Name = "values";
+            this.values.Width = 67;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите исходные данные:";
+            // 
+            // btnInterpolate
+            // 
+            this.btnInterpolate.Enabled = false;
+            this.btnInterpolate.Location = new System.Drawing.Point(29, 393);
+            this.btnInterpolate.Name = "btnInterpolate";
+            this.btnInterpolate.Size = new System.Drawing.Size(229, 72);
+            this.btnInterpolate.TabIndex = 4;
+            this.btnInterpolate.Text = "Найти формулу интерполяционного многочлена Лагранжа";
+            this.btnInterpolate.UseVisualStyleBackColor = true;
+            this.btnInterpolate.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            this.chart1.Location = new System.Drawing.Point(309, 92);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.MarkerColor = System.Drawing.Color.Red;
+            series3.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Cross;
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(328, 310);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(305, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(327, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Интерполяционный многочлен Лагранжа:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(305, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = " ";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Enabled = false;
+            this.btnCalculate.Location = new System.Drawing.Point(442, 408);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(169, 55);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "Найти значение в точке";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(309, 408);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(113, 26);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(305, 445);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 495);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.btnInterpolate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btnNumberOfPoints);
+            this.Name = "Form1";
+            this.Text = "Интерполяционный многочлен Лагранжа";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNumberOfPoints;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnInterpolate;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn arguments;
+        private System.Windows.Forms.DataGridViewTextBoxColumn values;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab5Graphic
+{
+    public partial class Form1 : Form
+    {
+        int n;
+        double[] x;
+        double[] fx;
+        double[] coeffs;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            n = ((int)numericUpDown1.Value);
+            dataGridView1.RowCount = n;
+            for (int i = 0; i < n; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = "0";
+                dataGridView1.Rows[i].Cells[1].Value = "0";
+            }
+            x = new double[n];
+            fx = new double[n];
+            coeffs = new double[n];
+            btnInterpolate.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            for (int i = 0; i < n; i++)
+            {
+                try
+                {
+                    x[i] = double.Parse(dataGridView1.Rows[i].Cells[0].Value.ToString());
+                    fx[i] = double.Parse(dataGridView1.Rows[i].Cells[1].Value.ToString());
+                }
+                catch (Exception) 
+                {
+                    MessageBox.Show("Неверно введены исходные данные в "+(i+1)+" строке");
+                    return;
+                }
+            }
+            //определение формулы интерполяционного многочлена Лагранжа
+            for (int i = 0; i < n; i++)
+            {
+                double d = 1;
+                double[]c = new double[n];
+                for (int l = 0; l < n; l++)
+                    c[l] = 1.0;
+                double[] c2 = new double[n];
+                for (int l = 0; l < n; l++)
+                    c2[l] = 1.0;
+                int m = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (i != j)
+                    {
+                        d *= (x[i] - x[j]);
+                        c[0] *= (-x[j]);
+                        if (m > 0) c[m] = 1;
+                        for (int k = 1; k < m + 1; k++)
+                            c[k] = c2[k] * (-x[j]) + c2[k - 1];
+                        for (int l = 0; l < n; l++)
+                            c2[l] = c[l];
+                        m++;
+                    }
+                }
+                for (int j = 0; j < n; j++)
+                {
+                    c[j] = c[j] * fx[i] / d;
+                    coeffs[j] += c[j];
+                }
+            }
+            //построение графика
+            chart1.ChartAreas[0].AxisX.Minimum = x[0];
+            chart1.ChartAreas[0].AxisX.Maximum = x[n-1];
+            chart1.Series[0].Points.DataBindXY(x, fx);
+            //вывод формулы на экран
+            string result = "L(x)=";
+            for (int i = n - 1; i >= 0; i--) { string str= string.Format("{0:+0.00;-0.00}x^{1:d}", coeffs[i], i);
+                result += str;
+            }
+            label3.Text = result;
+            btnCalculate.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double xx;
+            try
+            {
+                xx = double.Parse(textBox1.Text);
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Неверно введена точка");
+                return;
+            }
+            label4.Text = string.Format("{0:+0.00;-0.00}", L(xx));
+        }
+
+        private double L(double x) //вычисление значения многочлена Лагранжа в точке
+        {
+            double res = 0.0;
+            for (int i = 0; i < n; i++)
+                res += Math.Pow(x, i) * coeffs[i];
+            return res;
+        }
+    }
+}
